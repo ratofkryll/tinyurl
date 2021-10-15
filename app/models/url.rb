59bec78736e83ccfd501e055567bed93c44a1d3c
Gioanna Rheumer <ratofkryll@gmail.com>
2021-10-14 20:25:08 -0700

    create info path & update views

--- app/models/url.rb
+++ app/models/url.rb
@@ -8,6 +8,7 @@ class Url < ApplicationRecord
 
   validate :validate_url
 
+  # Validate to ensure the URL includes http:// or https://
   def is_valid_url?
     uri = URI.parse(self.long_url)
     return uri.is_a?(URI::HTTP) && !uri.host.nil?
@@ -16,7 +17,7 @@ class Url < ApplicationRecord
   end
 
   def validate_url
-    errors.add(:long_url, 'is invalid. Please make sure it begins with https:// or http://') unless is_valid_url?
+    errors.add(:long_url, 'is invalid.') unless is_valid_url?
   end
 
   # Convert base 10 to base 36 "short URL" - This creates a shorter URL, which is generated faster, and won't result in uniqueness conflicts

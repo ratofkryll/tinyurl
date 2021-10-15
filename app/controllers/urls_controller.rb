996092b35d7a77a202906a09e7139687a35f475a
Gioanna Rheumer <ratofkryll@gmail.com>
2021-10-14 21:56:08 -0700

    add new url form to root

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,6 +1,7 @@
 class UrlsController < ApplicationController
   def index
     @urls = Url.all
+    @url = Url.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: 'urls#new'
-
   resources :urls, only: [:index, :new, :show, :create], path: '/' do
     member do
       get :info

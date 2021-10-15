1e44d16fd874b577cbb94c37c75d592e11893aac
Gioanna Rheumer <ratofkryll@gmail.com>
2021-10-14 17:33:27 -0700

    create generate_short_url_token

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,13 @@
+require 'securerandom'
+
 class Url < ApplicationRecord
   has_many :visits
 
   # Validations
-  validates :long_url, presence: true
+  validates :long_url, presence: true, uniqueness: true
+
+  # Uses Ruby's SecureRandom library to generate an 8 character random string
+  def generate_short_url_token
+    SecureRandom.alphanumeric(8)
+  end
 end
